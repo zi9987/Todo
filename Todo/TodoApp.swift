cc3166609fb7882e533469a3e4d47d072bc7a581
zi9987 <peterluoluo25@gmail.com>
2024-05-04 23:26:12 +0800

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 羅子淵 on 2024/5/4.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
